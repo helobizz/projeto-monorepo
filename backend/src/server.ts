6a20b02215b2f72fb6ce4ce29e0691fdcfaf52e2
DSM4 <fatec-dsm4@academico.intra>
2026-08-10 22:26:18 -0300

    Primeiro commit'

---
+++ backend/src/server.ts
@@ -0,0 +1,38 @@
+import express, {Request, Response} from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv'
+import { sequelize } from './config/database';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware 
+app.use(cors());
+app.use(express.json());
+
+// Rota de Health Check (usa api para fazer checagem)
+app.get('/api/health', (req: Request, res: Response) => {
+    res.status(200).json({
+        status: 'OK',
+        messagem: 'Servidor Backend rodando com sucesso.',
+        timeStamp: new Date().toISOString
+    });
+});
+
+async function main() {
+    try {
+        await sequelize.authenticate();
+        console.log('Conexão com o PostGreSQL no supabase realizada com sucesso.');
+
+        app.listen(PORT, () => {
+            console.log(`Servidor rodando na porta ${PORT}`);
+            console.log(`Health Check disponível em: http://localhost:${PORT}/api/health`);
+        });
+    } catch (error) {
+        console.log('Erro ao conectar com o banco de dados: ', error);
+    }
+}
+
+main();
